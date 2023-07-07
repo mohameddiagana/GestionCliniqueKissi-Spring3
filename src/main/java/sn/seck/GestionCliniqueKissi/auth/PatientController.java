c2c3028608cca821dd70ef22c858720120ebb143
mohameddiagana <mohameddiagana807@gmail.com>
2023-07-07 09:26:57 +0000

    last coommit

--- src/main/java/sn/seck/GestionCliniqueKissi/auth/PatientController.java
+++ src/main/java/sn/seck/GestionCliniqueKissi/auth/PatientController.java
@@ -1,7 +1,6 @@
 package sn.seck.GestionCliniqueKissi.auth;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
